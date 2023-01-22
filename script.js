47e357a4f5951941bdf6e0feb9d00c20a92c241e
Charlie Hodgkinson <charlotte.hodgkinson4@gmail.com>
2023-01-22 17:07:04 +0000

    randomly generated circles on click

---
+++ script.js
@@ -0,0 +1,40 @@
+// clearing the stage
+function removeAll() {
+  document.getElementById("stage").innerHTML = "";
+}
+// removeAll();
+
+// calling my state svg
+let svg = document.getElementById("stage");
+
+function drawCircles() {
+  // how many circles to draw 
+  // I started with 8 but now I think 15 is the magic number
+  for (i = 0; i < 15; i++) {
+
+    // because circle isnt a html element I need to specify the SVG namespace 
+    circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+    // maximum 14, minimum 2 and in increments of 2 
+    circle.setAttribute("r", Math.floor(Math.random() * 7) * 2 + 2) ;
+
+    // minimum 20 maximum 80, incremements of 20 
+    // (so either 20 , 40, 60, 80 )
+    circle.setAttribute("cx", Math.floor(Math.random() * 4) * 20 + 20);
+    circle.setAttribute("cy", Math.floor(Math.random() * 4) * 20 + 20);
+
+    // add these elements to my stage
+    svg.appendChild(circle);
+  }
+}
+drawCircles();
+
+// var reGenTimer = window.setInterval(function () {
+//   removeAll();
+//   drawCircles();
+// }, 4000);
+
+document.addEventListener("click", function () {
+  removeAll();
+  drawCircles();
+});
